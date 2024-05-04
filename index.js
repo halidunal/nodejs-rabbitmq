c93ad6ff77e7d81d95962f468660289f782757d1
HalidÜnal <halidunal@gmail.com>
2024-05-04 14:27:57 +0300

    rabbitmq connection

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const PORT = 5000;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get("/",(req,res,next) => {
+  res.send("running")
+})
+
+router.post("/test",(req,res,next) => {
+  const {email} = req.body;
+  setTimeout(() => {
+    res.status(200).json({email: email, date: Date.now()})
+  }, 10000);
+})
+
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log("server is running on port:", PORT);
