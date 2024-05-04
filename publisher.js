cdbc9f0c7413292c45703432595f2e562d1d67e8
HalidÜnal <halidunal@gmail.com>
2024-05-04 14:33:38 +0300

    publisher

---
+++ publisher.js
@@ -0,0 +1,15 @@
+const amqp = require("amqplib");
+const rabbitmqConnection = require("./rabbitmqConnection");
+const queueName = "emailQueue"
+
+module.exports = async (email) => {
+    try {
+        const connection = await rabbitmqConnection;
+        const channel = await connection.createChannel();
+        await channel.assertQueue(queueName);
+        channel.sendQueue(queueName,Buffer.from(JSON.stringify({email: email})));
+        console.log("went to the queue")
+    } catch (error) {
+        console.log(error)
+    }
+}
